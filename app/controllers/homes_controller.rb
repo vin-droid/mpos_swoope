637b32a34ca9c5d430ac9274d73e649de3ecd2ca
Manoj Datt <km.arti@mobiloittegroup.com>
2016-04-12 09:55:15 +0530

    first commit add swoope html

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,15 @@
+class HomesController < ApplicationController
+  
+  def index
+  end
+
+  def how_it_works
+  end
+
+  def privacy_policy
+  end
+  
+  def terms_conditions
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  
+  #Statis Pages routes
+  root 'homes#index'
+  get '/index' => 'homes#index'
+  get '/how_it_works' => "homes#how_it_works"
+  get '/privacy_policy' =>  "homes#privacy_policy"
+  get '/terms_conditions' => "homes#terms_conditions"
+
+end
