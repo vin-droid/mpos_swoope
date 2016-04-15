77c7168c8528f3b872f43e2acc26b57f8c4747f8
Manoj Datt <km.arti@mobiloittegroup.com>
2016-04-15 09:08:15 +0530

    make it better

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -12,4 +12,14 @@ class HomesController < ApplicationController
   def terms_conditions
   end
 
+  def app_user_confirmation
+   	@user = User.find_by_confirmation_token(params[:confirmation_token])
+   	if @user.present?
+      @user.update_attributes(confirmation_token: nil ,confirmed_at: Time.now.utc)
+      m =  "Your account activated successfully, Please go to the app and press login to use the app." 
+    else
+      m = "Your account already Activated, Please go to the app and press login to use the app."
+    end
+    redirect_to how_it_works_path, notice: m
+ 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get '/how_it_works' => "homes#how_it_works"
   get '/privacy_policy' =>  "homes#privacy_policy"
   get '/terms_conditions' => "homes#terms_conditions"
+  get '/app_user_confirmation/:confirmation_token' => "homes#app_user_confirmation" 
 
 end
